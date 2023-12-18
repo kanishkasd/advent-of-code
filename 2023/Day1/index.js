8980ffc4f23c5272d3c581b16ee4ae6cbe9bdac1
kanishkasd <kanishkasandaru2048@gmail.com>
2023-12-18 21:09:47 +0530

    completed Day1 first part

---
+++ 2023/Day1/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+
+function calculateSum(filePath) {
+    let sum = 0;
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return err;
+        }
+
+        const lines = data.trim().split('\n');
+
+        const values = lines.map((line) => {
+            const numbers = line.match(/\d/g);
+
+            let first = numbers[0];
+            let last = numbers[numbers.length - 1];
+
+            return Number(first + last);
+        });
+        const total = values.reduce((value, sum) => value + sum);
+        console.log(total);
+    })
+}
+
+
+calculateSum('./input.txt');
