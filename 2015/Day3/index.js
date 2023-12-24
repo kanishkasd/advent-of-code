cccf4f155abc5804672237e4df7ae41ca65fbc7b
kanishkasd <kanishkasandaru2048@gmail.com>
2023-12-24 20:28:23 +0530

    completed the 2015 day3 part 1

---
+++ 2015/Day3/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function housesWithPresents(directions) {
+    const visitedHouses = new Map();
+    let x = 0;
+    let y = 0;
+
+    function visitHouse(x, y) {
+        const key = x + ',' + y;
+        visitHouse.set(key, (visitedHouses.get(key) || 0) + 1);
+    }
+    visitHouse(x, y);
+
+    for (let direction of directions) {
+        const data = direction.trim();
+        switch (data) {
+            case '^':
+                y++;
+                break;
+            case 'v':
+                y--;
+                break;
+            case '>':
+                x++;
+                break;
+            case '<':
+                x--;
+                break;
+        }
+        visitHouse(x, y);
+    }
+
+    return visitedHouses.size;
+}
