dae01013ef08dc9c7fb5c1895cd5c2dbb4842a22
kanishkasd <kanishkasandaru2048@gmail.com>
2023-12-26 21:21:35 +0530

    day5 2015

---
+++ 2015/Day5/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function findNiceStrings(){
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let badStrings = ['ab', 'cd', 'pq', 'xy'];
+
+    let niceStrings = 0;
+    
+}
+
+
+
+
+
+
+
+
+
+fs.readFile('./file.txt', 'utf8', (err, data) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+})
