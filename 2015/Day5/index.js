0c8e045e25c1f1c6581756340a284940367148a8
kanishkasd <kanishkasandaru2048@gmail.com>
2023-12-29 06:15:43 +0530

    2015 day 5 first part

--- 2015/Day5/index.js
+++ 2015/Day5/index.js
@@ -1,24 +1,36 @@
 const fs = require('fs');
 
-function findNiceStrings(){
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-    let badStrings = ['ab', 'cd', 'pq', 'xy'];
+function isNiceString(string) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let vowelCount = string.split('').filter(letter => vowels.includes(letter)).length;
+    const badStrings = ['ab', 'cd', 'pq', 'xy'];
 
-    let niceStrings = 0;
-    
+    const hasForbidden = badStrings.some(badString => string.includes(badString));
+
+    function hasConsecutiveChars() {
+        for (let i = 0; i < string.length - 1; i++) {
+                if (string[i] === string[i + 1]) {
+                    return true;
+                }
+            }
+        }
+    return vowelCount >= 3 && !hasForbidden && hasConsecutiveChars();
 }
 
-
-
-
-
-
-
-
-
-fs.readFile('./file.txt', 'utf8', (err, data) => {
-    if(err){
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
         console.log(err);
         return;
     }
+    const lines = data.split('\n');
+    let niceStrings = 0;
+
+    lines.forEach(line => {
+        if (isNiceString(line)) {
+            niceStrings++;
+        }
+    })
+    console.log(niceStrings);
+
+
 })
