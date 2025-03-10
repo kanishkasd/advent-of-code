11c63281e4134cf6566128433eafbd28503199c7
kanishkasd <kanishkasandaru2048@gmail.com>
2025-03-10 16:37:14 +0530

    completed the day 1 second part

--- 2017/Day 1/script.js
+++ 2017/Day 1/script.js
@@ -11,7 +11,23 @@ const calculateSum = (data) => {
   const repeatedDigitsNums = repeatedDigits.map(Number);
 
   const sum = repeatedDigitsNums.reduce((acc, num) => acc + num, 0);
-  console.log(sum);
+  // console.log(sum);
+};
+
+///////////////////////////////////// 2nd problem //////////////////////////////////
+
+const matchingNumberSum = (data) => {
+  const dataArray = data.split("").map(Number);
+
+  const halfLength = dataArray.length / 2;
+
+  const matchingNum = dataArray.filter((num, index, arr) => {
+    const nextIndex = (index + halfLength) % arr.length;
+    return num === arr[nextIndex];
+  });
+
+  const sumOfMatchingNums = matchingNum.reduce((acc, num) => acc + num, 0);
+  console.log(sumOfMatchingNums);
 };
 
 fs.readFile("input.txt", "utf-8", (err, data) => {
@@ -19,5 +35,5 @@ fs.readFile("input.txt", "utf-8", (err, data) => {
     console.log(err);
     return;
   }
-  calculateSum(data);
+  matchingNumberSum(data);
 });
