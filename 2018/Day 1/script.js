c35f73327d126550e89052d5afd44060ceedc19f
kanishkasd <kanishkasandaru2048@gmail.com>
2025-03-11 21:20:17 +0530

    completed the day 1

--- 2018/Day 1/script.js
+++ 2018/Day 1/script.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 
-fs.readFile("./input.txt", "utf-8", (data, err) => {
+fs.readFile("./input.txt", "utf-8", (err, data) => {
   if (err) {
     console.log(err);
   }
+
+  const sum = data
+    .split(/\s+/)
+    .map(Number)
+    .reduce((acc, num) => acc + num, 0);
+
+  console.log("Sum : ", sum);
 });
